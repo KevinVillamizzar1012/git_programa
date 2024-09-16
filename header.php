49be1b1a68c6dad98d3b124ac8c1e6da612eac03
KevinVillamizzar1012 <villamizzar1012@gmail.com>
2024-09-16 10:18:03 -0500

    Resueltos los conflictos

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Administración Hotel</title>
+</head>
+<body>
+    <header>
+        <img src="./img/Newhome.jpg" alt="Logotipo del sitio">
+        <h1>Panel de Administración Hotel</h1>
+    </header>
+    
+    <?php include('menu-principal.php');
+    include('contenido.php'); ?>
